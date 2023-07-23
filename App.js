216beaa6db1a84fdfc83b5e93974b55414c32de5
Apinnn <117737878+Despinnn@users.noreply.github.com>
2023-07-23 19:03:30 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,124 @@
-import { StatusBar } from 'expo-status-bar';
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Text, Image, StyleSheet, StatusBar} from 'react-native';
+import AppIntroSlider from 'react-native-app-intro-slider';
+import Slider from './assets/Slider';
+import MyTabs from './assets/BottomTabNav';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
+type Item = (typeof dataSlide)[0];
 const styles = StyleSheet.create({
-  container: {
+  slide: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  image: {
+    width: 250,
+    height: 250,
+    marginVertical: 32,
+  },
+  text: {
+    color: 'rgba(255, 255, 255, 0.8)',
+    textAlign: 'center',
+    paddingVertical: 20,
+    fontSize: 20,
+  },
+  title: {
+    fontSize: 34,
+    color: 'white',
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: 'rgba(255, 0, 0, 1)',
+    color: '#FFF',
+  },
+  flex1: {
+    flex: 4,
+    justifyContent: 'flex-end',
+  },
+  flex2: {
+    flex: 2,
   },
 });
+
+class App extends React.Component {
+state = {
+    showRealApp: false,
+  };
+
+  _renderItem = ({item}: {item: Item}) => {
+    return (
+      <View
+        style={[
+          styles.slide,
+          {
+            backgroundColor: item.bg,
+          },
+        ]}>
+        <View style={styles.flex1}>
+          <Image style={styles.image} source={item.image} />
+        </View>
+
+        <View style={styles.flex2}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.text}>{item.text}</Text>
+        </View>
+      </View>
+    );
+  };
+
+  _keyExtractor = (item: Item) => item.title;
+
+  _renderPrevButton = () => {
+    return (
+      <View style={styles.button}>
+        <Text style={styles.button}>Prev</Text>
+      </View>
+    );
+  };
+
+  _renderNextButton = () => {
+    return (
+      <View style={styles.button}>
+        <Text style={styles.button}>Next</Text>
+      </View>
+    );
+  };
+
+  _renderDoneButton = () => {
+    return (
+      <View style={styles.button}>
+        <Text style={styles.button}>Done</Text>
+      </View>
+    );
+  };
+
+  _onDone = () => {
+    this.setState({showRealApp: true});
+  };
+
+  render() {
+    if (this.state.showRealApp) {
+      return (
+        <NavigationContainer>
+        <MyTabs />
+        </NavigationContainer>
+      );
+    } else {
+      return (
+        <View style={{flex: 1}}>
+          <StatusBar translucent backgroundColor="transparent" />
+          <AppIntroSlider
+            keyExtractor={this._keyExtractor}
+            renderItem={this._renderItem}
+            showPrevButton
+            onDone={this._onDone}
+            data={Slider}
+          />
+        </View>
+      );
+    }
+  }
+}
+
+export default App;
